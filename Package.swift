7b18714879c56886a5112144980e1c1ff655a0be
Theo <mail@theo.sh>
2024-10-27 10:59:34 +0100

    Update package url

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             name: "QuiltString",
             targets: ["QuiltString"]),
     ],
-    dependencies: [.package(path: "../Quilt")],
+    dependencies: [.package(url: "https://github.com/theolampert/Quilt", branch: "main")],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
